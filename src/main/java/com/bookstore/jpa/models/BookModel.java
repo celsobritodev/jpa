220972881ecfbd2d4194df341f2b27e55251f56c
celsobritodev <celsinhobrito@gmail.com>
2024-07-19 10:34:53 -0300

    jpa com spring

---
+++ src/main/java/com/bookstore/jpa/models/BookModel.java
@@ -0,0 +1,104 @@
+package com.bookstore.jpa.models;
+
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+
+
+@Entity
+@Table(name="TB_BOOK")
+public class BookModel implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private UUID id;
+	
+	@Column(nullable=false, unique=true)
+	private String title;
+	
+	//@JsonProperty(access=JsonProperty.Access.WRITE_ONLY)
+	@ManyToOne//(fetch=FetchType.LAZY)
+	@JoinColumn(name="publisher_id")
+	private PublisherModel publisher; // relacionamento com editora
+	
+	//@JsonProperty(access=JsonProperty.Access.WRITE_ONLY)
+	@ManyToMany//(fetch=FetchType.LAZY)
+	@JoinTable(
+			name="tb_book_author",
+			joinColumns=@JoinColumn(name="book_id"),
+			inverseJoinColumns=@JoinColumn(name="author_id"))
+	private Set<AuthorModel> authors = new HashSet<>();
+	
+	@OneToOne(mappedBy="book", cascade=CascadeType.ALL)
+	private ReviewModel review; // resumo do livro
+
+	
+	
+	
+	
+	public UUID getId() {
+		return id;
+	}
+
+	public void setId(UUID id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+	public PublisherModel getPublisher() {
+		return publisher;
+	}
+
+	public void setPublisher(PublisherModel publisher) {
+		this.publisher = publisher;
+	}
+
+	public Set<AuthorModel> getAuthors() {
+		return authors;
+	}
+
+	public void setAuthors(Set<AuthorModel> authors) {
+		this.authors = authors;
+	}
+
+	public ReviewModel getReview() {
+		return review;
+	}
+
+	public void setReview(ReviewModel review) {
+		this.review = review;
+	}
+	
+	
+	
+
+}
